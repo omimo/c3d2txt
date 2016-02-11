97ed3b38ea87f10988f0678ba2d00af3cb480433
Omid <omid.alemi@gmail.com>
2016-02-11 00:07:00 -0800

    Initial Commit

---
+++ C3DtoTxt/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C3DtoTxt
+//
+//  Created by Omid on 2016-02-11.
+//  Copyright © 2016 Omid. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
